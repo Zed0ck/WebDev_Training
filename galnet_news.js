21c95a2bfb5152de6e3a26c7211407430b5cff89
Zed0ck <mika.vallin@gmail.com>
2019-12-05 15:35:19 +0200

    Changes to galnet_news.js

--- galnet_news.js
+++ galnet_news.js
@@ -16,11 +16,14 @@ const section = document.querySelector('section');
                     myDiv1.className = 'news';
                     section.appendChild(myDiv1);
                     
-
+                    const myH3 = document.createElement('h3');
+                    myH3.textContent = jsonObj[i].date;
+                    myDiv1.appendChild(myH3);
+                    
                     const myH2 = document.createElement('h2');
                     myH2.textContent = jsonObj[i].title;
                     myDiv1.appendChild(myH2);
-    
+
                     const myDiv2 = document.createElement('div');
                     myDiv2.innerHTML = jsonObj[i].body;
                     myDiv1.appendChild(myDiv2);
