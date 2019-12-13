c6b011cca000e1d97edc3b41ceac798019dd60d5
Zed0ck <mika.vallin@gmail.com>
2019-12-13 14:18:20 +0200

    essentials.js added

---
+++ essentials.js
@@ -0,0 +1,4 @@
+document.getElementById("pageNav").innerHTML = "<ul>"
+    + "<li><a href='index.html'>Home</a></li>"
+    + "<li><a href='#'>News</a></li>"
+    + "<li><a href='#'>About</a></li></ul>";
